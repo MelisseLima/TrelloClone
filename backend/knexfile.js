7b56c36635badc127920786a8164ce3fadf0c787
Melisse Cabral <melissecabral@gmail.com>
2020-09-09 14:44:28 -0300

    add variable enviroment database_url

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,65 +1,55 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: 'localhost',
-      username: 'mel',
-      password: 'amor',
-      database: 'my_db',
+      host: "localhost",
+      username: "mel",
+      password: "amor",
+      database: "my_db",
     },
     migrations: {
-      directory: './src/database/migrations',
+      directory: "./src/database/migrations",
     },
     useNullAsDefault: true,
   },
 
   test: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: 'localhost',
-      username: 'postgres',
-      password: 'amor',
-      database: 'my_db',
+      host: "localhost",
+      username: "postgres",
+      password: "amor",
+      database: "my_db",
     },
     migrations: {
-      directory: './src/database/migrations',
+      directory: "./src/database/migrations",
     },
     useNullAsDefault: true,
   },
 
   staging: {
-    client: 'pg',
-    connection: {
-      database: 'my_db',
-      user: 'postgres',
-      password: '',
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations',
+      tableName: "knex_migrations",
     },
   },
 
   production: {
-    client: 'pg',
-    connection: {
-      database: 'my_db',
-      user: 'postgres',
-      password: '',
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations',
+      tableName: "knex_migrations",
     },
   },
-
 };
