6febdc5ddef9086d45a2a3fd135866951126b3dd
melissecabral <melissecabral@gmail.com>
2021-05-30 11:38:08 -0300

    Back config server

--- packages/frontend/src/services/api.js
+++ packages/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://apitrelloclone.herokuapp.com',
   headers: {
     Authorization: `Bearer ${sessionStorage.getItem('jwt')}`,
   },
