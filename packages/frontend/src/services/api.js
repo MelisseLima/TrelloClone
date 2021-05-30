d454a13dec4959eb7f1d2f837ed4e24b705a6e23
melissecabral <melissecabral@gmail.com>
2021-05-30 11:37:08 -0300

    Add typescript at backend

--- packages/frontend/src/services/api.js
+++ packages/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8282',
+  baseURL: 'http://localhost:3333',
   headers: {
     Authorization: `Bearer ${sessionStorage.getItem('jwt')}`,
   },
